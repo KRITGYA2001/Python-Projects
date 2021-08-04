520f3720cfeb747230f82cc72226e6fdbd35208d
KRITGYA2001 <55615841+KRITGYA2001@users.noreply.github.com>
2021-08-04 15:16:54 +0530

    Add files via upload

---
+++ Face and Eye Detection/Face and Eye Detection.py
@@ -0,0 +1,67 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[6]:
+
+
+# OpenCV program to detect face in real time
+# import libraries of python OpenCV 
+import cv2
+
+# load the required trained XML classifiers
+# from a lot of positive(faces) and negative(non-faces)
+face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+
+# Trained XML file for detecting eyes
+eye_cascade = cv2.CascadeClassifier('haarcascade_eye.xml') 
+
+# capture frames from a camera
+cap = cv2.VideoCapture(0)
+
+# loop runs if capturing has been initialized.
+while 1:
+
+    # reads frames from a camera
+    ret, img = cap.read()
+
+    # convert to gray scale of each frames
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+    # Detects faces of different sizes in the input image
+    faces = face_cascade.detectMultiScale(gray, 1.3, 5)
+    
+    # Detects eyes of different sizes in the input image
+    eyes = eye_cascade.detectMultiScale(roi_gray) 
+
+    #To draw a rectangle in a face 
+    for (x,y,w,h) in faces:
+        cv2.rectangle(img,(x,y),(x+w,y+h),(255,255,0),2)
+        roi_gray = gray[y:y+h, x:x+w]
+        roi_color = img[y:y+h, x:x+w]
+        
+        
+    #To draw a rectangle in eyes
+    for (ex,ey,ew,eh) in eyes:
+        cv2.rectangle(roi_color,(ex,ey),(ex+ew,ey+eh),(0,127,255),2)    
+
+    
+    #Display an image in a window
+    cv2.imshow('img',img)
+
+    #Wait for Esc key to stop
+    k = cv2.waitKey(30) & 0xff
+    if k == 27:
+        break
+
+#Close the window
+cap.release()
+
+#De-allocate any associated memory usage
+cv2.destroyAllWindows()
+
+
+# In[ ]:
+
+
+
+
---
+++ Face and Eye Detection/README.md
@@ -0,0 +1,6 @@
+# Face-and-Eye-Detection-
+Face and Eye Detection Program Using Python Modules (cv2)
+
+
+Download All Three files and paste them in one folder.
+Open Face_and_Eye_Detection.py file into your editor/Compiler and run.
